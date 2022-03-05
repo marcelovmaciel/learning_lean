f2678eccd75036dfecae72e92ca7363c7bd72f0a
marcelovmaciel <marcelovmaciel@protonmail.com>
2022-03-05 01:07:01 -0800

    did two exercises. gonna do way more tomorrow

--- theorem_proving_with_lean/second_notes_exercies.lean
+++ theorem_proving_with_lean/second_notes_exercies.lean
@@ -20,8 +20,6 @@ example : p ∨ q ↔ q ∨ p := iff.intro
     (assume hp : p, or.inl  hp))
 
 
-
-
 -- * associativity of ∧ and ∨
 example : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r) := iff.intro
 (assume h1 :(p ∧ q) ∧ r,
@@ -37,11 +35,26 @@ or.elim h1
      or.elim hpq
        (assume hp, or.inl hp)
        (assume hq, or.inr (or.inl hq))) -- that was tricky wtf
-     (assume hr, or.inr (or.inr hr)))
-(assume h2, _)
+  (assume hr, or.inr (or.inr hr)))
+(assume h2,
+  h2.elim
+  (assume hp, or.inl (or.inl hp) )
+  (assume hqr, hqr.elim
+    (assume hq, or.inl (or.inr hq))
+    (assume hr, or.inr hr)))
 
 -- distributivity
-example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := sorry
+example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := iff.intro
+(assume h1,
+  have hp:p, from h1.left,
+  h1.right.elim
+    (assume hq, or.inl ⟨hp,hq⟩)
+    (assume hr, or.inr ⟨hp, hr⟩) )
+(assume h2, h2.elim
+  (assume hpq,
+    ⟨hpq.left, or.inl hpq.right⟩)
+  (assume hpr, ⟨hpr.left, or.inr hpr.right⟩))
+
 example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) := sorry
 
 -- other properties
