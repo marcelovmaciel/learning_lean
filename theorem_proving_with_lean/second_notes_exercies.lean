1dc6712c30089d5912a1b7a55fd0b9897f010b33
marcelovmaciel <marcelovmaciel@protonmail.com>
2022-03-05 15:21:11 -0800

    pump up the jam https://www.youtube.com/watch?v=9EcjWd-O4jI

--- theorem_proving_with_lean/second_notes_exercies.lean
+++ theorem_proving_with_lean/second_notes_exercies.lean
@@ -49,23 +49,95 @@ example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := iff.intro
   have hp:p, from h1.left,
   h1.right.elim
     (assume hq, or.inl ⟨hp,hq⟩)
-    (assume hr, or.inr ⟨hp, hr⟩) )
+    (assume hr, or.inr ⟨hp, hr⟩))
 (assume h2, h2.elim
   (assume hpq,
     ⟨hpq.left, or.inl hpq.right⟩)
   (assume hpr, ⟨hpr.left, or.inr hpr.right⟩))
 
-example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) := sorry
-
+example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) := iff.intro
+(assume h1, h1.elim
+  (assume hp, ⟨or.inl hp, or.inl hp⟩)
+  (assume hqr,⟨or.inr hqr.left, or.inr hqr.right⟩)) --
+(assume h2,
+  h2.left.elim
+    (assume hp, or.inl hp)
+    (assume hq,
+      h2.right.elim
+        (assume hp,or.inl hp )
+        (assume hr, or.inr ⟨hq,hr⟩)))
 -- other properties
-example : (p → (q → r)) ↔ (p ∧ q → r) := sorry
-example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) := sorry
-example : ¬(p ∨ q) ↔ ¬p ∧ ¬q := sorry
-example : ¬p ∨ ¬q → ¬(p ∧ q) := sorry
-example : ¬(p ∧ ¬p) := sorry
-example : p ∧ ¬q → ¬(p → q) := sorry
-example : ¬p → (p → q) := sorry
-example : (¬p ∨ q) → (p → q) := sorry
-example : p ∨ false ↔ p := sorry
-example : p ∧ false ↔ false := sorry
-example : (p → q) → (¬q → ¬p) := sorry
+example : (p → (q → r)) ↔ (p ∧ q → r) := iff.intro
+  (assume h1,
+    assume pq,
+      h1 pq.left pq.right) -- (assume h1, λ pq, (h1 pq.1 pq.2 )) was my first try.
+  (assume h2, assume ifp, assume ifq, h2 ⟨ifp, ifq⟩)
+
+example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) :=
+  iff.intro
+    (assume h1,
+      and.intro   -- this one is very relevant, I had
+          (assume hp, h1 (or.inl hp)) -- gotten stuck with the and in the goal!
+          (assume hq, h1 (or.inr hq)))
+    (assume h2,
+      assume porq,
+        porq.elim
+          (assume hp, h2.left hp)
+          (assume hq, h2.right hq))
+
+example : ¬(p ∨ q) ↔ ¬p ∧ ¬q :=
+  iff.intro
+    (assume h1,
+      and.intro
+        (assume hp, h1 (or.inl hp))
+        (assume hq, h1 (or.inr hq)))
+    (assume h2,
+      assume porq,
+             porq.elim
+               (assume hp, h2.left hp)
+               (assume hq, h2.right hq))
+
+
+example : ¬p ∨ ¬q → ¬(p ∧ q) :=
+  assume h1,
+  assume h2,
+  h1.elim
+    (assume notp, notp h2.left)
+    (assume notq, notq h2.right)
+
+example : ¬(p ∧ ¬p) :=
+  assume h1, absurd h1.left h1.right
+
+example : p ∧ ¬q → ¬(p → q) :=
+  assume hp_and_nq,
+  assume hp_then_q,
+    hp_and_nq.right (hp_then_q hp_and_nq.left)
+
+example : ¬p → (p → q) :=
+  assume hnotp,
+  assume hp,
+    absurd hp hnotp
+
+example : (¬p ∨ q) → (p → q) :=
+  assume hnotp_or_q,
+  assume hp,
+    hnotp_or_q.elim
+      (assume hnotp, absurd hp hnotp)
+      (assume hq, hq)
+
+example : p ∨ false ↔ p :=
+  iff.intro
+  (assume hp_or_false, hp_or_false.elim
+    (assume hp,hp)
+    (assume f, f.elim))
+  (assume hp, or.inl hp)
+
+example : p ∧ false ↔ false :=
+  iff.intro
+  (assume hp_and_false, hp_and_false.right)
+  (assume f, f.elim)
+
+example : (p → q) → (¬q → ¬p) :=
+  assume hifp_then_q,
+  assume hnot_q,
+  assume hp,  absurd (hifp_then_q hp) hnot_q
